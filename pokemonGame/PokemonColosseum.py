2118de4f3b8736a0a79134af93e7558f9dbd5bd9
crewsdel <124068095+crewsdel@users.noreply.github.com>
2024-01-31 14:02:26 -0500

    yuh

---
+++ pokemonGame/PokemonColosseum.py
@@ -0,0 +1,7 @@
+
+print('Welcome to Pokemon Colosseum!\n')
+player_team = input('Enter Player Name\n')
+print('Team Rocket enters with BLANK BLANK BLANK\n')
+print('Team' + player_team + 'enters with BLANK BLANK\n')
+
+print('Let the battle begin!\n')
