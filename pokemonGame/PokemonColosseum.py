c4019552dc931365229d6e178e227ad21d3a03b9
crewsdel <124068095+crewsdel@users.noreply.github.com>
2024-02-01 18:06:38 -0500

    yea

--- pokemonGame/PokemonColosseum.py
+++ pokemonGame/PokemonColosseum.py
@@ -1,7 +1,6 @@
 import csv
 import ast
 import random
-
 from pokemonGame.Pokemon import Pokemon
 
 pokemon_filename = 'pokemon-data.csv'
@@ -49,27 +48,59 @@ with open(pokemon_filename) as csvfile:
 
 rocket_pokemon = random.sample(pokemon_box, 3)
 player_pokemon = random.sample(pokemon_box, 3)
-computer = 'Team Rocket'
 
+computer = 'Team Rocket'  # Maybe I do not need this
+all_poke_dead = 'false'
 
-def coin_toss():
-    if random.randint(0, 1) < 0.5:
-        return player_team
-    else:
-        return computer
-
-
-
-
+coin_toss = random.randint(0, 1)
 
 print('Welcome to Pokemon Colosseum!\n')
 player_team = input('Enter Player Name\n')
 
 print(f"Team Rocket enters with: {', '.join(str(pokemon.name) for pokemon in rocket_pokemon)}\n")
-
 print(f"Team {player_team} enters with: {', '.join(str(pokemon.name) for pokemon in player_pokemon)}\n")
 
 print('Let the battle begin!\n')
 
-winner = coin_toss()
-print(f'Coin toss goes to ----- {winner} to start the attack')
+
+# winner = 'Team Rocket'  # coin_toss()
+def is_alive(self):
+    return self.health > 0
+
+
+if coin_toss == 0:
+    winner = player_team
+    loser = 'Team Rocket'
+
+else:
+    winner = 'Rocket'
+    loser = player_team
+
+print(f'Coin toss goes to ----- Team {winner} to start the attack!')
+
+computer_current_pokemon = rocket_pokemon.pop(0)
+player_current_pokemon = player_pokemon.pop(0)
+
+
+def battle(player_current_pokemon, computer_current_pokemon):
+    computer_current_pokemon = rocket_pokemon.pop(0)
+    player_current_pokemon = player_pokemon.pop(0)
+    while len(player_pokemon) != 0 and len(rocket_pokemon) != 0:
+
+        gather_moves = pokemon_moves.get(computer_current_pokemon.name)
+        random_attack = random.sample(gather_moves, 1)
+        print(f"Team Rocket's {computer_current_pokemon.name} attacks with: {random_attack}")
+
+    else:
+
+
+        print(player_current_pokemon.name)
+        print(f'Choose the move for {player_current_pokemon.name}')
+        gather_moves = pokemon_moves.get(player_current_pokemon.name)
+        print(gather_moves)
+
+    if len(rocket_pokemon) == 0 & len(player_pokemon) == 0:
+        exit()
+
+
+battle(player_current_pokemon, computer_current_pokemon)
